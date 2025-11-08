ddfb7e1bf6d1cbac0ca15ef5f1f5648bcab48f5e
TrinityNET <lazarosjake@gmail.com>
2025-11-08 19:00:05 +0000

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Car
+    {
+        public string Make { get; private set; }
+        public string Model { get; private set; }
+
+        public Car(string make, string model)
+        {
+            Make = make;
+            Model = model;
+        }
+
+        public string getMake()
+        {
+            return Make;
+        }
+
+        public string getModel()
+        {
+            return Model;
+        }
+    }
+}
